b8c183f7e3c4fdf22c6fafba8667be9b1ac43d72
guanghuhappysf128 <ghu1@student.unimelb.edu.au>
2024-11-23 21:34:00 +1100

    some change.

--- src/arduino_comm/scripts/arduino_serial.py
+++ src/arduino_comm/scripts/arduino_serial.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Bool
+import serial
+from std_msgs.msg import Bool, Int32
+
+ser = serial.Serial('/dev/ttyUSB0',115200, timeout=1)
+
+def timer_callback(event):
+    if ser.in_waiting>0:
+        incoming_byte = ser.read(1)
+        if incoming_byte:
+            number = int.from_bytes(incoming_byte, byteorder='little')
+            rospy.loginfo("Received:{number}")
+            UI_pub.publish(number)
+
 
 def button_callback(msg):
     """
@@ -10,32 +22,36 @@ def button_callback(msg):
     """
     if msg.data:
         rospy.loginfo("Button has been pressed!")
+        ser.write(bytes[3])
+
     else:
         rospy.loginfo("Button state is false.")
 
-def led_toggle_publisher():
+def listener():
     """
     Publishes to the 'led' topic every 10 seconds to toggle the LED state.
     """
-    rospy.init_node('led_toggle_node', anonymous=True)
-    led_pub = rospy.Publisher('led', Bool, queue_size=10)
+    # rospy.init_node('led_toggle_node', anonymous=True)
+    rospy.init_node('ui_comm_node', anonymous=True)
 
-    button_sub = rospy.Subscriber('button_pressed', Bool, button_callback)
+    # led_pub = rospy.Publisher('led', Bool, queue_size=10)
+    global UI_pub
+    UI_pub = rospy.Publisher('UI', Int32, queue_size=10)
 
-    rate = rospy.Rate(0.1)  # 0.1 Hz = every 10 seconds
-    led_state = False
+    button_sub = rospy.Subscriber('button', Bool, button_callback)
+    timer = rospy.Timer(rospy.Duration(1), timer_callback)
+    # rate = rospy.Rate(0.1)  # 0.1 Hz = every 10 seconds
+    # led_state = False
 
-    rospy.loginfo("LED toggle publisher is running.")
+    rospy.spin
 
-    while not rospy.is_shutdown():
-        led_state = not led_state  # Toggle LED state
-        led_pub.publish(led_state)
-        rospy.loginfo(f"Published LED state: {'ON' if led_state else 'OFF'}")
-        rate.sleep()
+    # rospy.loginfo("LED toggle publisher is running.")
+
+    # while not rospy.is_shutdown():
+    #     led_state = not led_state  # Toggle LED state
+    #     led_pub.publish(led_state)
+    #     rospy.loginfo(f"Published LED state: {'ON' if led_state else 'OFF'}")
+    #     rate.sleep()
 
 if __name__ == "__main__":
-    try:
-        led_toggle_publisher()
-    except rospy.ROSInterruptException:
-        pass
-
+    listener()
